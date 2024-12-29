ddaadc836d74ee212aa6df35e3e659bfe7cdff51
JToTheThree <johnmullins@gmail.com>
2024-12-28 21:25:22 -0600

    Bevy 0.15 upgrade

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,6 +1,7 @@
 use bevy::{
     dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
     prelude::*,
+    text::FontSmoothing,
 };
 
 use bevy_northstar::prelude::*;
@@ -19,7 +20,7 @@ pub struct Stats {
 }
 
 #[derive(Resource, Debug, Default)]
-pub struct Config { 
+pub struct Config {
     use_astar: bool,
 }
 
@@ -28,8 +29,10 @@ impl Stats {
         self.pathfind_time.push(time);
         self.pathfind_length.push(length);
 
-        self.average_time = self.pathfind_time.iter().sum::<f64>() / self.pathfind_time.len() as f64;
-        self.average_length = self.pathfind_length.iter().sum::<f64>() / self.pathfind_length.len() as f64;
+        self.average_time =
+            self.pathfind_time.iter().sum::<f64>() / self.pathfind_time.len() as f64;
+        self.average_length =
+            self.pathfind_length.iter().sum::<f64>() / self.pathfind_length.len() as f64;
         self.count += 1;
     }
 
@@ -60,14 +63,13 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(FpsOverlayPlugin {
             config: FpsOverlayConfig {
-                text_config: TextStyle {
-                    // Here we define size of our overlay
+                text_config: TextFont {
                     font_size: 32.0,
-                    // We can also change color of the overlay
-                    color: Color::srgb(0.0, 1.0, 0.0),
-                    // If we want, we can use a custom font
+                    font_smoothing: FontSmoothing::default(),
                     font: default(),
                 },
+                text_color: Color::srgb(0.0, 1.0, 0.0),
+                enabled: true,
             },
         })
         // bevy_ecs_tilemap and bevy_ecs_tiled main plugins
@@ -100,7 +102,7 @@ fn main() {
             jump_height: 1,
         }))
         // Observe the LayerCreated event to build the grid
-        .observe(layer_created)
+        .add_observer(layer_created)
         // Add our systems and run the app!
         .add_systems(Startup, startup)
         .add_systems(OnEnter(State::Playing), spawn_minions)
@@ -110,7 +112,10 @@ fn main() {
         .add_systems(Update, pathfind_minions.run_if(in_state(State::Playing)))
         .add_systems(Update, set_new_goal.run_if(in_state(State::Playing)))
         .add_systems(Update, update_stat_text.run_if(in_state(State::Playing)))
-        .add_systems(Update, update_pathfind_type_test.run_if(in_state(State::Playing)))
+        .add_systems(
+            Update,
+            update_pathfind_type_test.run_if(in_state(State::Playing)),
+        )
         .add_event::<Tick>()
         .insert_state(State::Loading)
         .insert_resource(Walkable::default())
@@ -124,7 +129,7 @@ struct Tick;
 
 // Generate a tick event
 fn tick(time: Res<Time>, mut tick_writer: EventWriter<Tick>) {
-    if time.elapsed_seconds() % 0.1 < time.delta_seconds() {
+    if time.elapsed_secs() % 0.1 < time.delta_secs() {
         tick_writer.send_default();
     }
 }
@@ -135,10 +140,9 @@ struct StatText;
 #[derive(Component, Debug)]
 struct PathfindTypeText;
 
-
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Spawn a 2D camera (required by Bevy)
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d);
 
     // Load the map ...
     let map_handle: Handle<TiledMap> = asset_server.load("demo_128.tmx");
@@ -158,86 +162,65 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
     ));
 
-    // Pathfinding Type Text
-    let pathfinding_text_style = TextStyle {
-        // Here we define size of our overlay
-        font_size: 32.0,
-        // We can also change color of the overlay
-        color: Color::srgb(0.0, 1.0, 0.0),
-        // If we want, we can use a custom font
-        font: default(),
-    };
-
-    commands.spawn(NodeBundle {
-        style: Style {
-            // We need to make sure the overlay doesn't affect the position of other UI nodes
-            position_type: PositionType::Absolute,
-            bottom: Val::Px(50.0),
-            left: Val::Px(0.0),
-            ..default()
-        },
-        // Render overlay on top of everything
-        z_index: ZIndex::Global(i32::MAX - 32),
-        ..default()
-    }).with_children(|c| {
-        c.spawn((
-            TextBundle::from_sections([
-                TextSection::new("Key [P] Algorithm: ", pathfinding_text_style.clone()),
-                TextSection::from_style(pathfinding_text_style),
-            ]),
+    commands
+        .spawn((
+            Text::new("Key [p]| Algorithm: "),
+            TextFont {
+                font_size: 24.0,
+                ..default()
+            },
+            Node {
+                position_type: PositionType::Absolute,
+                bottom: Val::Px(50.0),
+                left: Val::Px(0.0),
+                ..default()
+            },
+        ))
+        .with_child((
+            TextSpan::default(),
+            TextFont {
+                font_size: 24.0,
+                ..default()
+            },
             PathfindTypeText,
         ));
-    });
-
-    // Stats Text
-    let stat_text_style = TextStyle {
-        // Here we define size of our overlay
-        font_size: 32.0,
-        // We can also change color of the overlay
-        color: Color::srgb(0.0, 1.0, 0.0),
-        // If we want, we can use a custom font
-        font: default(),
-    };
 
     commands
-        .spawn(NodeBundle {
-            style: Style {
-                // We need to make sure the overlay doesn't affect the position of other UI nodes
+        .spawn((
+            Text::new("Avg Path Time: "),
+            TextFont {
+                font_size: 24.0,
+                ..default()
+            },
+            Node {
                 position_type: PositionType::Absolute,
                 bottom: Val::Px(100.0),
                 left: Val::Px(0.0),
                 ..default()
             },
-            // Render overlay on top of everything
-            z_index: ZIndex::Global(i32::MAX - 32),
-            ..default()
-        })
-        .with_children(|c| {
-            c.spawn((
-                TextBundle::from_sections([
-                    TextSection::new("Avg Path Time: ", stat_text_style.clone()),
-                    TextSection::from_style(stat_text_style),
-                ]),
-                StatText,
-            ));
-        });
+        ))
+        .with_child((
+            TextSpan::default(),
+            TextFont {
+                font_size: 24.0,
+                ..default()
+            },
+            StatText,
+        ));
 }
 
-fn update_stat_text(
-    stats: Res<Stats>,
-    mut query: Query<(&mut Text, &StatText)>,
-) {
-    for (mut text, _) in query.iter_mut() {
-        text.sections[1].value = format!("{:.2}ms", stats.average_time * 1000.0);
+fn update_stat_text(stats: Res<Stats>, mut query: Query<&mut TextSpan, With<StatText>>) {
+    for mut span in &mut query {
+        **span = format!("{:.2}ms", stats.average_time * 1000.0);
     }
 }
 
 fn update_pathfind_type_test(
     config: Res<Config>,
-    mut query: Query<(&mut Text, &PathfindTypeText)>,
+    mut query: Query<&mut TextSpan, With<PathfindTypeText>>,
 ) {
-    for (mut text, _) in query.iter_mut() {
-        text.sections[1].value = if config.use_astar {
+    for mut span in &mut query {
+        **span = if config.use_astar {
             "A*".to_string()
         } else {
             "HPA*".to_string()
@@ -268,7 +251,9 @@ fn move_pathfinders(
             let position = Vec3::new(next.x as f32 * 8.0 + 4.0, next.y as f32 * 8.0 + 4.0, 4.0);
             minion.position = next;
 
-            commands.entity(entity).insert(Transform::from_translation(position));
+            commands
+                .entity(entity)
+                .insert(Transform::from_translation(position));
         }
     }
 }
@@ -295,22 +280,20 @@ fn pathfind_minions(
         let goal = goal.position;
 
         let now = std::time::Instant::now();
-        
+
         let path = if config.use_astar {
             grid.get_astar_path(start, goal)
-        } else { 
+        } else {
             grid.get_path(start, goal)
         };
-        
+
         let elapsed = now.elapsed().as_secs_f64();
 
         if path.is_none() {
-            info!("No path found for {:?} from {:?} to {:?}", entity, start, goal);
-            let path = if config.use_astar {
-                grid.get_astar_path(start, goal)
-            } else { 
-                grid.get_path(start, goal)
-            };
+            info!(
+                "No path found for {:?} from {:?} to {:?}",
+                entity, start, goal
+            );
             continue;
         }
 
@@ -359,26 +342,27 @@ fn spawn_minions(
         }
     }
 
-
-
     let mut count = 0;
 
     while count <= 128 {
         let position = walkable.tiles.choose(&mut rand::thread_rng()).unwrap();
         let goal = walkable.tiles.choose(&mut rand::thread_rng()).unwrap();
 
-
         let transform = Vec3::new(position.x + 4.0, position.y + 4.0, 4.0);
 
-        commands.spawn(SpriteBundle {
-            texture: asset_server.load("tile_0018.png"),
-            transform: Transform::from_translation(transform),
-            ..Default::default()
-        }).insert(Minion {
-            position: UVec3::new((position.x / 8.0) as u32, (position.y / 8.0) as u32, 0), 
-        }).insert(Goal {
-            position: UVec3::new((goal.x / 8.0) as u32, (goal.y / 8.0) as u32, 0),
-        }).set_parent(layer_entity);
+        commands
+            .spawn(Sprite {
+                image: asset_server.load("tile_0018.png"),
+                ..Default::default()
+            })
+            .insert(Transform::from_translation(transform))
+            .insert(Minion {
+                position: UVec3::new((position.x / 8.0) as u32, (position.y / 8.0) as u32, 0),
+            })
+            .insert(Goal {
+                position: UVec3::new((goal.x / 8.0) as u32, (goal.y / 8.0) as u32, 0),
+            })
+            .set_parent(layer_entity);
 
         count += 1;
     }
@@ -409,9 +393,15 @@ fn layer_created(
                     let tile_id = tile.unwrap().id();
 
                     if tile_id == 14 {
-                        grid.set_point(UVec3::new(x as u32, height - 1 - y as u32, 0), Point::new(1, false));
+                        grid.set_point(
+                            UVec3::new(x as u32, height - 1 - y as u32, 0),
+                            Point::new(1, false),
+                        );
                     } else {
-                        grid.set_point(UVec3::new(x as u32, height - 1 - y as u32, 0), Point::new(0, true));
+                        grid.set_point(
+                            UVec3::new(x as u32, height - 1 - y as u32, 0),
+                            Point::new(0, true),
+                        );
                     }
                 }
             }
@@ -468,7 +458,7 @@ pub fn input(
         }
 
         let z = transform.translation.z;
-        transform.translation += time.delta_seconds() * direction * 500.;
+        transform.translation += time.delta_secs() * direction * 500.;
         // Important! We need to restore the Z values when moving the camera around.
         // Bevy has a specific camera setup and this can mess with how our layers are shown.
         transform.translation.z = z;
